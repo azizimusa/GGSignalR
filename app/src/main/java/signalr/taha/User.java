a3a21cdc848f584fa5352ad41f926de48a7ede9d
azizimusa@azizi2u.com <Aswaja77>
2020-11-22 11:51:16 +0800

    initial commit

---
+++ app/src/main/java/signalr/taha/User.java
@@ -0,0 +1,13 @@
+package signalr.taha;
+
+public class User {
+
+    public String userName;
+    public String connectionID;
+
+    public User(String userName, String connectionID) {
+        this.userName = userName;
+        this.connectionID = connectionID;
+    }
+
+}
